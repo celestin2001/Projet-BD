0e2b5062aa8c96ff3ffe47b7dca52302c20ac6d0
celestin <jeremiesandouno123@gmail.com>
2025-03-17 08:32:19 +0000

    change

--- Gestion_BD/gestion_utilisateur/models.py
+++ Gestion_BD/gestion_utilisateur/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from  gestion_content.models import Genre,Work
+
 
 class Utilisateur(AbstractUser):
     Role_choice = [
